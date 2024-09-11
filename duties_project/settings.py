01e8329551b0134770c6a94aa88db2bb4bd90dba
rolczynska <alexandra.rolczynska@gmail.com>
2024-09-11 11:06:40 +0200

    Add Django Token Auth

--- duties_project/settings.py
+++ duties_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'family_duties',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
--- family_duties/urls.py
+++ family_duties/urls.py
@@ -5,6 +5,8 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework.authtoken import views
+
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
@@ -15,4 +17,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
